785013ab7dac5cb2c60793b45d310f0bd92b3580
Pablo Machado <machadoum@gmail.com>
2017-06-04 22:53:20 -0300

    Add move and scale image feature

--- app/js/main.js
+++ app/js/main.js
@@ -2,23 +2,39 @@ const readFile = require('./read-file')
 const {
   onGenerate,
   onSelectFile,
+  onMoveLeft,
+  onMoveRight,
+  onMoveUp,
+  onMoveDown,
+  onScaleUp,
+  onScaleDown,
   log,
   clearImageContainer,
   loadImage,
   showImage,
-  getImageProperties,
-  DPI
+  pixelsToInches
 } = require('./view')
 
+const MOVE_STEP = 0.3
+const SCALE_STEP = 0.1
 const AVAILABLE_IMAGE_TYPES = ['image/jpeg', 'image/png', 'image/gif']
 const isValidImage = (file) => AVAILABLE_IMAGE_TYPES.includes(file.type)
 let loadedImage = undefined
 let loadedFile = undefined
+const initialStore = {
+  x: 0,
+  y: 0,
+  width: 0,
+  height: 0
+}
+let store = Object.assign({}, initialStore)
 
 const resetState = () => {
   clearImageContainer(imageContainer)
   loadedImage = undefined
   loadedFile = undefined
+
+  store = Object.assign({}, initialStore)
 }
 
 onSelectFile((file) => {
@@ -30,21 +46,55 @@ onSelectFile((file) => {
 
   readFile(file)
     .then(loadImage)
-    .then(showImage)
-    .then((image) => {
-      loadedImage = image
-      log('Loaded Image w/dimensions ' + image.naturalWidth + ' x ' + image.naturalHeight)
+    .then((img) => {
+      loadedImage = img
+      store.width = pixelsToInches(img.naturalWidth)
+      store.height = pixelsToInches(img.naturalHeight)
+      showImage(img, store)
+
+      log('Loaded Image w/dimensions ' + img.naturalWidth + ' x ' + img.naturalHeight)
     })
 })
 
 onGenerate(() => {
   if(!loadedImage) return log('No image loaded')
 
-  const generatedData = Object.assign(
-    { id: loadedFile.name },
-    getImageProperties(loadedImage))
+  const generatedData = Object.assign({ id: loadedFile.name }, store)
 
   log('Generated Image:' + JSON.stringify(generatedData))
 })
+
+onMoveLeft(() => {
+  store.x = store.x - MOVE_STEP
+  showImage(loadedImage, store)
+})
+
+onMoveRight(() => {
+  store.x = store.x + MOVE_STEP
+  showImage(loadedImage, store)
+})
+
+onMoveUp(() => {
+  store.y = store.y - MOVE_STEP
+  showImage(loadedImage, store)
+})
+
+onMoveDown(() => {
+  store.y = store.y + MOVE_STEP
+  showImage(loadedImage, store)
+})
+
+onScaleUp(() => {
+  store.width = store.width + store.width * SCALE_STEP
+  store.height = store.height + store.height * SCALE_STEP
+  showImage(loadedImage, store)
+})
+
+onScaleDown(() => {
+  store.width = store.width - store.width * SCALE_STEP
+  store.height = store.height - store.height * SCALE_STEP
+  showImage(loadedImage, store)
+})
+
 
 log('Test application ready')
--- app/js/view.js
+++ app/js/view.js
@@ -1,19 +1,30 @@
-const fileSelector = document.getElementById( 'fileSelector' )
-const imageContainer = document.getElementById( 'imageContainer' )
-const debugContainer = document.getElementById( 'debugContainer' )
-const generateButton = document.getElementById( 'generateButton' )
-const SCALE = 1/3
-const CANVAS_WIDTH = 15 // inch
-const CANVAS_HEIGH = 10 // inch
-const DPI = imageContainer.offsetHeight / (CANVAS_HEIGH * SCALE)
+const fileSelector = document.getElementById('fileSelector')
+const imageContainer = document.getElementById('imageContainer')
+const ctx = imageContainer.getContext("2d")
+const debugContainer = document.getElementById('debugContainer')
+const generateButton = document.getElementById('generateButton')
+const moveLeftButton = document.getElementById('move-left')
+const moveRightButton = document.getElementById('move-right')
+const moveUpButton = document.getElementById('move-up')
+const moveDownButton = document.getElementById('move-down')
+const scaleUpButton = document.getElementById('scale-up')
+const scaleDownButton = document.getElementById('scale-down')
 
-imageContainer.style.width = `${CANVAS_WIDTH * SCALE}in`
-imageContainer.style.height = `${CANVAS_HEIGH * SCALE}in`
+const SCALE = 1/3
+const CANVAS_WIDTH = 15 * SCALE // inch
+const CANVAS_HEIGH = 10 * SCALE // inch
+const DPI = 96
+
+const inchesToPixels = (inches) => inches * DPI
+const pixelsToInches = (pixels) => pixels / DPI
+
+imageContainer.width = inchesToPixels(CANVAS_WIDTH)
+imageContainer.height = inchesToPixels(CANVAS_HEIGH)
+
 
 const log = (msg) => debugContainer.innerHTML += '<p>' + msg + '</p>'
 
-const clearImageContainer = (container) => {
-  ctx = imageContainer.getContext("2d")
+const clearImageContainer = () => {
   ctx.clearRect(0, 0, imageContainer.width, imageContainer.height)
 }
 
@@ -24,33 +35,41 @@ const loadImage = (imageData) =>
     img.src = imageData
   })
 
-const showImage = (img) => {
-  ctx = imageContainer.getContext("2d")
-  ctx.drawImage(img, 0, 0)
-  return img
-}
+const showImage = (img, {x, y, width, height} = {}) => {
+  clearImageContainer()
 
-const getImageProperties = (img) => {
-  return {
-    'width': img.naturalWidth / DPI,
-    'height': img.naturalHeight / DPI,
-    'x': 0 / DPI,
-    'y': 0 / DPI
-  }
+  ctx.drawImage(img,
+    inchesToPixels(x) * SCALE,
+    inchesToPixels(y) * SCALE,
+    inchesToPixels(width) * SCALE,
+    inchesToPixels(height) * SCALE
+  )
 }
 
 const onSelectFile = (onSelect) =>
   fileSelector.onchange = (event) => onSelect(event.target.files[0])
 
 const onGenerate = (onGenerate) => generateButton.onclick = onGenerate
+const onMoveLeft = (onClick) => moveLeftButton.onclick = onClick
+const onMoveRight = (onClick) => moveRightButton.onclick = onClick
+const onMoveUp = (onClick) => moveUpButton.onclick = onClick
+const onMoveDown = (onClick) => moveDownButton.onclick = onClick
+const onScaleUp = (onClick) => scaleUpButton.onclick = onClick
+const onScaleDown = (onClick) => scaleDownButton.onclick = onClick
 
 module.exports = {
   log,
   clearImageContainer,
   showImage,
-  getImageProperties,
   onSelectFile,
   onGenerate,
   loadImage,
-  DPI
+  onMoveLeft,
+  onMoveRight,
+  onMoveUp,
+  onMoveDown,
+  onScaleUp,
+  onScaleDown,
+  inchesToPixels,
+  pixelsToInches,
 }
